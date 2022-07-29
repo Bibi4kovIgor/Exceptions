4dca9607e6f345e928cd6783b4ba1699cab85702
Bibi4kovIgor <work.bibi4kov@gmail.com>
2022-07-29 19:42:38 +0300

    Library added

--- src/main/java/launcher/Application.java
+++ src/main/java/launcher/Application.java
@@ -2,14 +2,30 @@ package launcher;
 
 import custom.FileManager;
 import custom.IncorrectFileNameException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
+
 public class Application {
-    public static void main(String[] args) throws IncorrectFileNameException, IOException {
-        String str = Paths.get("src",  "main", "resources", "test.txt").toString();
-        System.out.println(FileManager.getFirstLine(str));
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.error("Some error");
+
+        String str = Paths.get("src",  "main", "resources", "tes1t.txt").toString();
+        try {
+            System.out.println(FileManager.getFirstLine(str));
+            System.out.println("Some data");
+        } catch (IncorrectFileNameException e) {
+            logger.warn(e.getMessage());
+//            logger.warn(Arrays.toString(e.getStackTrace()));
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("Hello World");
+        }
     }
 }
---
+++ src/test/java/custom/FileManagerTest.java
@@ -0,0 +1,14 @@
+package custom;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FileManagerTest {
+
+    @Test
+    void getFirstLine() {
+        assertTrue(true);
+    }
+}
